4e40a738e21291b5830c41c1fbbbf7142e261842
datashaman <marlinf@datashaman.com>
2014-03-05 23:13:03 +0200

    Merge options into config

--- mars.rkt
+++ mars.rkt
@@ -7,18 +7,23 @@
 
 (define default-instruction (redcode-instruction '() 'DAT '() '$ '0 '$ '0))
 
+(define (merge-config options)
+  (make-hash (append (list (cons 'core-size 8000)
+                           (cons 'pspace-size 500)
+                           (cons 'max-cycles 80000)
+                           (cons 'max-processes 8000)
+                           (cons 'max-length 100)
+                           (cons 'min-distance 100))
+                     (hash->list options))))
+
 (define (make-core warriors options)
-  (let* ([config (hash "core-size" 8000
-                        "pspace-size" 500
-                        "max-cycles" 80000
-                        "max-processes" 8000
-                        "max-length" 100
-                        "min-distance" 100)])
-    (core warriors (make-vector (hash-ref config "core-size") default-instruction))))
+  (let* ([config (merge-config options)]
+         [instructions (make-vector (hash-ref config 'core-size) default-instruction)])
+    (core warriors instructions)))
 
 (define (first-core options)
   (let ([imp (redcode-parse "imp" (open-input-file "warriors/imp.redcode"))]
         [dwarf (redcode-parse "dwarf" (open-input-file "warriors/dwarf.redcode"))])
     (make-core (list imp dwarf) options)))
 
-(print (first-core (hash "core-size" 100)))
+(print (first-core (hash 'core-size 100)))
